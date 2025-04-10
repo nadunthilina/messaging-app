0fb54134e9e2e27e479cb9684bb31aa48093390c
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-10 08:55:21 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
