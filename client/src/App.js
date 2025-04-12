c39f37eba3a6707d4b9b4c627ee551ff165245c1
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-12 09:14:34 +0530

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Register from './components/Register';
+import Chat from './components/Chat';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+        <Chat user={user} />
+      ) : (
+        <div>
+          <Login setUser={setUser} />
+          <Register />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
