4ee097cd5be593ff7ac29acd50800cd3425a43c3
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-13 09:18:54 +0530

    Add a profilePhoto

--- server/models/User.js
+++ server/models/User.js
@@ -4,3 +4,11 @@ const userSchema = new mongoose.Schema({
   password: String, // In a real app, hash passwords
 });
 module.exports = mongoose.model('User', userSchema);
+
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  profilePhoto: { type: String, default: '' }, // Store as base64 or URL
+});
+module.exports = mongoose.model('User', userSchema);
