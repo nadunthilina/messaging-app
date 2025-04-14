7da880793f8bcb5554676fd04c970959c48b6c50
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-14 07:15:55 +0530

    update message.js

--- server/routes/message.js
+++ server/routes/message.js
@@ -3,17 +3,25 @@ const router = express.Router();
 const Message = require('../models/Message');
 
 router.get('/:user', async (req, res) => {
-  const messages = await Message.find({
-    $or: [{ sender: req.params.user }, { recipient: req.params.user }],
-  }).sort({ timestamp: 1 });
-  res.json(messages);
+  try {
+    const messages = await Message.find({
+      $or: [{ sender: req.params.user }, { recipient: req.params.user }],
+    }).sort({ timestamp: 1 });
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 router.post('/', async (req, res) => {
   const { sender, recipient, content } = req.body;
-  const message = new Message({ sender, recipient, content });
-  await message.save();
-  res.status(201).send('Message sent');
+  try {
+    const message = new Message({ sender, recipient, content });
+    await message.save();
+    res.status(201).json({ message: 'Message sent' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
